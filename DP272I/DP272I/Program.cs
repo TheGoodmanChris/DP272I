c6f21b8e401e0fe226be9cef824a7934bdb83dce
Chris Goodman <thegoodmanchris@gmail.com>
2016-06-27 22:34:16 -0700

    Fixed error propogation, added FloydSteinberg mode and greyscale mode.

--- DP272I/DP272I/Program.cs
+++ DP272I/DP272I/Program.cs
@@ -17,7 +17,9 @@ namespace DP272I
     {
         static void Main(string[] args)
         {
-            Bitmap OriginalImage = new Bitmap(@"C:\Users\cgoodman\Source\Repos\DP272I\DP272I\DP272I\kjWn2Q1.png");
+            string mode = "FloydSteinberg";
+
+            Bitmap OriginalImage = new Bitmap(@"C:\Users\coper\Source\Repos\DP272I\DP272I\DP272I\kjWn2Q1.png");
             Bitmap newImage = new Bitmap(OriginalImage.Width, OriginalImage.Height);
 
             for(int y = 0; y < OriginalImage.Height; y++)
@@ -27,21 +29,119 @@ namespace DP272I
                     Color pixel = OriginalImage.GetPixel(x, y);
                     byte greyscaleVal = (byte)((pixel.R + pixel.G + pixel.B) / 3);
 
-                    if(greyscaleVal < 128)
+                    if (mode == "Grey")
                     {
-                        newImage.SetPixel(x, y, Color.Black);
-
-                        byte errorDiff = (byte)(128 - greyscaleVal);
-                        if (x+1 <= OriginalImage.Width)
-                        {
-                            Color errorPropogation = OriginalImage.GetPixel(x + 1, y);
-                            //OriginalImage.SetPixel(x + 1, y, OriginalImage.GetPixel(x + 1, y)
-
-                        }
+                        Color greyscaleColor = Color.FromArgb((int)greyscaleVal, (int)greyscaleVal, (int)greyscaleVal);
+                        newImage.SetPixel(x, y, greyscaleColor);
                     }
 
+                    if(mode == "FloydSteinberg")
+                    {
+                        if (greyscaleVal < 128)
+                        {
+                            newImage.SetPixel(x, y, Color.Black);
+
+                            int errorDiff = greyscaleVal;
+
+                            if (x + 1 < OriginalImage.Width)
+                            {
+                                float FloydSteinbergConstant = 7f / 16;
+                                Color originalNextPixel = OriginalImage.GetPixel(x + 1, y);
+                                Color newNextPixel = nextPixel(FloydSteinbergConstant, originalNextPixel, errorDiff);
+
+                                OriginalImage.SetPixel(x + 1, y, newNextPixel);
+                            }
+
+                            if (x - 1 >= 0 && y + 1 < OriginalImage.Height)
+                            {
+                                float FloydSteinbergConstant = 3f / 16;
+                                Color originalNextPixel = OriginalImage.GetPixel(x - 1, y + 1);
+                                Color newNextPixel = nextPixel(FloydSteinbergConstant, originalNextPixel, errorDiff);
+
+                                OriginalImage.SetPixel(x - 1, y + 1, newNextPixel);
+                            }
+
+                            if (y + 1 < OriginalImage.Height)
+                            {
+                                float FloydSteinbergConstant = 5f / 16;
+                                Color originalNextPixel = OriginalImage.GetPixel(x, y + 1);
+                                Color newNextPixel = nextPixel(FloydSteinbergConstant, originalNextPixel, errorDiff);
+
+                                OriginalImage.SetPixel(x, y + 1, newNextPixel);
+                            }
+
+                            if (x + 1 < OriginalImage.Width && y + 1 < OriginalImage.Height)
+                            {
+                                float FloydSteinbergConstant = 1f / 16;
+                                Color originalNextPixel = OriginalImage.GetPixel(x + 1, y + 1);
+                                Color newNextPixel = nextPixel(FloydSteinbergConstant, originalNextPixel, errorDiff);
+
+                                OriginalImage.SetPixel(x + 1, y + 1, newNextPixel);
+                            }
+                        }
+
+                        else
+                        {
+                            newImage.SetPixel(x, y, Color.White);
+
+                            int errorDiff = greyscaleVal - 255;
+
+                            if (x + 1 < OriginalImage.Width)
+                            {
+                                float FloydSteinbergConstant = 7f / 16;
+                                Color originalNextPixel = OriginalImage.GetPixel(x + 1, y);
+                                Color newNextPixel = nextPixel(FloydSteinbergConstant, originalNextPixel, errorDiff);
+
+                                OriginalImage.SetPixel(x + 1, y, newNextPixel);
+                            }
+
+                            if (x - 1 >= 0 && y + 1 < OriginalImage.Height)
+                            {
+                                float FloydSteinbergConstant = 3f / 16;
+                                Color originalNextPixel = OriginalImage.GetPixel(x - 1, y + 1);
+                                Color newNextPixel = nextPixel(FloydSteinbergConstant, originalNextPixel, errorDiff);
+
+                                OriginalImage.SetPixel(x - 1, y + 1, newNextPixel);
+                            }
+
+                            if (y + 1 < OriginalImage.Height)
+                            {
+                                float FloydSteinbergConstant = 5f / 16;
+                                Color originalNextPixel = OriginalImage.GetPixel(x, y + 1);
+                                Color newNextPixel = nextPixel(FloydSteinbergConstant, originalNextPixel, errorDiff);
+
+                                OriginalImage.SetPixel(x, y + 1, newNextPixel);
+                            }
+
+                            if (x + 1 < OriginalImage.Width && y + 1 < OriginalImage.Height)
+                            {
+                                float FloydSteinbergConstant = 1f / 16;
+                                Color originalNextPixel = OriginalImage.GetPixel(x + 1, y + 1);
+                                Color newNextPixel = nextPixel(FloydSteinbergConstant, originalNextPixel, errorDiff);
+
+                                OriginalImage.SetPixel(x + 1, y + 1, newNextPixel);
+                            }
+                        }
+                    }                    
                 }
             }
+
+            newImage.Save(@"C:\Users\coper\Source\Repos\DP272I\DP272I\DP272I\shapesOutput.png");
+        }
+
+        static int Clamp(int val, int min, int max)
+        {
+            if (val.CompareTo(min) < 0) return min;
+            else if (val.CompareTo(max) > 0) return max;
+            else return val;
+        }
+
+        static Color nextPixel(float FloydSteinbergConstant, Color originalPixel, int errorDiff)
+        {
+            byte newGreyscaleVal = (byte)((originalPixel.R + originalPixel.G + originalPixel.B) / 3);
+            int newRGB = (int)(Clamp((int)(newGreyscaleVal + (errorDiff * FloydSteinbergConstant)), 0, 255));
+
+            return Color.FromArgb(newRGB, newRGB, newRGB);
         }
     }
 }
